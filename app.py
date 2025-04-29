c1354d838b8f36bc0407392daa395ba06ce7ab4c
Fabio Chicout <fchicout@gmail.com>
2025-04-29 16:06:34 -0300

    creating class to abstract MQTT stream to siddhi

--- app.py
+++ app.py
@@ -2,13 +2,32 @@ from time import sleep
 from manager import SiddhiAppManager
 from query_callback import QueryCallbackImpl
 from sender import EventSender
+from mqtt_stream import MQTTStream 
 
+# Criação dinâmica da definição de stream usando MQTTStream
+mqtt_stream = MQTTStream("cseEventStream")
+mqtt_stream.add_mqtt_attribute("mqtt_symbol", "string")
+mqtt_stream.add_mqtt_attribute("mqtt_price", "float")
+mqtt_stream.add_mqtt_attribute("mqtt_volume", "long")
+
+# Geração da string Siddhi com stream dinâmico
+stream_definition = mqtt_stream.defineStreamString()
+print(stream_definition);
 SIDDHI_APP = (
-    "define stream cseEventStream (symbol string, price float, volume long); "
+    stream_definition + "; "
+
     "@info(name = 'query1') "
     "from cseEventStream[volume < 150] "
     "select symbol,price insert into outputStream;"
 )
+
+
+# SIDDHI_APP = (
+#     "define stream cseEventStream (symbol string, price float, volume long); "
+#     "@info(name = 'query1') "
+#     "from cseEventStream[volume < 150] "
+#     "select symbol,price insert into outputStream;"
+# )
 
 def main():
     manager = SiddhiAppManager(SIDDHI_APP)
